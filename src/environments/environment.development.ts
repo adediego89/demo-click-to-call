6064325159b3d3da10d7b7a380ca57ef7a9b35c4
Asier de Diego <asier.dediego@whirly.pl>
2025-10-17 13:56:08 +0200

    domain change

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  domain: 'byoc.webrtc.whirly.pl',
+  domain: 'webrtc.whirly.pl',
   addresses: ['wss://webrtc.whirly.pl'],
   iceServers: ['74.125.140.127:19302', '74.125.143.127:19302']
 };
